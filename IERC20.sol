1bd80233ae4da70dea160082df89bd791f6eb186
Civa <44102586+civa@users.noreply.github.com>
2022-05-09 09:42:12 +0100

    Create IERC20.sol

---
+++ IERC20.sol
@@ -0,0 +1,11 @@
+interface IERC20 {
+    function transfer(address to, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function totalSupply() external view returns (uint);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
